16310b4654eac872002908c0b31f5d69413661ab
unknown <kalyanraju90@gmail.com>
2021-05-18 22:33:11 +0530

    outline of site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import HomePage from './homepage.component'
+import React, {Component} from 'react'
 import './App.css';
+import './homepage.style.scss'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <HomePage/>
     </div>
-  );
+  )
 }
 
 export default App;
