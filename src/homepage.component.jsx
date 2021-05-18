ac52bee1f8b25325107d9c7e234f169a091bcb8f
unknown <kalyanraju90@gmail.com>
2021-05-18 22:40:12 +0530

    outline

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -3,7 +3,7 @@ import './homepage.style.scss'
 
 const HomePage = () =>(
     <div className='homepage'>
-        <div className='directory-menu'>
+        <div className='directory'>
             <div className="menu-item">
                 <div className="content">
                     <h1 className="title">Hats</h1>
