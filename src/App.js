fc13a7e39d25c9ae0cfda79862cd5396fdc965d2
unknown <kalyanraju90@gmail.com>
2021-05-23 00:23:47 +0530

    collection-item and its styling

--- src/App.js
+++ src/App.js
@@ -2,23 +2,14 @@ import HomePage from './pages/homepage/homepage.component'
 import React, {Component} from 'react'
 import './App.css';
 import {Switch, Route} from 'react-router-dom'
-
-
-
-const Hats = () => (
-
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-
-);
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return(
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route path='/hats' component= { Hats }/>
+        <Route path='/shop' component= {ShopPage}/>
       </Switch>
       
     </div>
