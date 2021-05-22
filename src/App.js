0bfc77f0dbfdb46590136b333badaea36445aaf1
unknown <kalyanraju90@gmail.com>
2021-05-22 22:55:37 +0530

    onclick on menu to render hats component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import HomePage from './pages/homepage/homepage.component'
 import React, {Component} from 'react'
 import './App.css';
+import {Switch, Route} from 'react-router-dom'
 
+
+
+const Hats = () => (
+
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+
+);
 
 function App() {
   return(
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/hats' component= { Hats }/>
+      </Switch>
+      
     </div>
   )
 }
